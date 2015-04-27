7e81afdcffa20a5f49525a214f06e2267d481e50
Denis Kozlov <deniskozlov@outlook.com>
2015-04-27 21:48:10 +0400

    Added DbServerType Enum and tool using description

---
+++ src/win/scafftools/makedb/DbServerTypeEnum.cs
@@ -0,0 +1,26 @@
+﻿
+namespace scafftools.makedb
+{
+	/// <summary>
+	/// Database server type
+	/// </summary>
+	public enum DbServerTypeEnum
+	{
+		/// <summary>
+		/// Microsoft SQL Server
+		/// </summary>
+		mssql,
+		/// <summary>
+		/// MySQL
+		/// </summary>
+		mysql,
+		/// <summary>
+		/// PostgreSQL
+		/// </summary>
+		postgre,
+		/// <summary>
+		/// Oracle DB
+		/// </summary>
+		oracle
+	}
+}
---
+++ src/win/scafftools/makedb/Readme.txt
@@ -0,0 +1,13 @@
+﻿use makedb.exe to generate database scheme and generate db
+
+makedb -s mssql -c "server=.\localhost;database=test" -f C:\project\*.mkdb
+
+ -s - database server type:
+		mssql	- Microsoft SQL Server
+		mysql	- MySQL
+		postgre - PostgreSQL 
+		oracle	- Oracle DB
+ -c - connection string
+ -f - force scheme/db rewrite
+
+ C:\project\*.mkdb - path to .mkdb file(s)
